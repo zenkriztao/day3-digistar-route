a1e8229a2ac03541beab7a7d7dd1ef9da509c17f
Muhammad Rizki <Zenkriztao3@gmail.com>
2024-09-04 20:47:18 +0700

    chore: refactor mongodb :fire:

---
+++ src/domain/models/models.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { User } from "../../core/types";
+
+const userSchema = new Schema<User & Document>({
+  userId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+export const UserModel = mongoose.model<User & Document>("User", userSchema);
